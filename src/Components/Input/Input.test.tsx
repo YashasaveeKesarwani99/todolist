c7ef119b51b5cc6cc2ac3cdb307557abbcac29ee
YashasaveeKesarwani99 <yashasvikesarwani1@gmail.com>
2022-01-13 23:41:02 +0530

    tests on Input added

--- src/Components/Input/Input.test.tsx
+++ src/Components/Input/Input.test.tsx
@@ -3,18 +3,40 @@ import { render,screen,fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import Input from './Input'
 
+const mockSetTask = jest.fn()
+
 describe('Testing Input Component',()=>{
 
-    var setTask:React.Dispatch<React.SetStateAction<string>>
     var setTasks:React.Dispatch<React.SetStateAction<string[]>>
-    var task:string = ""
-    var tasks:string[]=[]
+    var task:string
+    var tasks:string[] = []
 
     it('Input area is present',()=>{
-        render(<Input  setTask={setTask} setTasks={setTasks} tasks={tasks} task={task}/>)
+        render(<Input  setTask={mockSetTask} setTasks={setTasks} tasks={tasks} task={task}/>)
         const ele = screen.getByPlaceholderText("Enter Task")
 
         expect(ele).toBeInTheDocument()
     })
+
+
+    it('gets input from input component',  ()=>{
+        render(<Input  setTask={mockSetTask} setTasks={setTasks} tasks={tasks} task={task}/>)
+        const ele = screen.getByPlaceholderText("Enter Task") as HTMLInputElement
+
+        fireEvent.change(ele,{target:{value:'Go To Gym'}})
+        expect(ele.value).toBe('Go To Gym')
+    })
+
+    it('input element gets cleared when button clicked',  ()=>{
+        render(<Input  setTask={mockSetTask} setTasks={mockSetTask} tasks={tasks} task={task}/>)
+        const ele = screen.getByPlaceholderText("Enter Task") as HTMLInputElement
+        const but = screen.getByTestId("button")
+
+        fireEvent.change(ele,{target:{value:'Go To Gym'}})
+        fireEvent.click(but)
+        fireEvent.change(ele,{target:{value:''}})
+        expect(ele.value).toBe('')
+    })
+
 
 })
